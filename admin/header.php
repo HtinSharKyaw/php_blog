eb943e787c3bd21a8e7f5c6a371a0ea750c29cfb
Htin Shar Kyaw <htinsharrkyaw@gmail.com>
2020-11-20 01:44:14 +0630

    search problem with token error solved

--- admin/header.php
+++ admin/header.php
@@ -32,7 +32,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
       </ul>
-      <?php 
+      <?php
         $link = $_SERVER['PHP_SELF'];//getting the current link
         $linkArray = explode('/',$link);//transforming the link into the array
         $page = end($linkArray);
@@ -40,6 +40,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
       <!-- SEARCH FORM -->
       <form class="form-inline ml-3" method="POST" action="<?php echo $page =="index.php"? 'index.php':'userview.php' ?>">
+        <input type="hidden" name="token" value="<?php echo $_SESSION['_token'];?>">
         <div class="input-group input-group-sm">
           <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" name="searchName">
           <div class="input-group-append">
@@ -59,7 +60,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="../dist/img/AdminLTELogo.png" alt="admin Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">Blog panel</span>
       </a>
